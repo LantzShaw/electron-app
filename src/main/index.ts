ac439f5c84d18345cf3a0d09b92ea25b7c8cbfbd
Lantz <lantzshaw@163.com>
2024-12-08 21:53:13 +0800

    feat: add multi windows

--- src/main/index.ts
+++ src/main/index.ts
@@ -18,7 +18,8 @@ function createWindow(): void {
       sandbox: false,
       // 在 BrowserWindow 中，始终设置 nodeIntegration: false 来确保渲染进程无法直接访问 Node.js API，这样可以增强安全性。preload.js 是唯一允许渲染进程与主进程进行交互的安全通道
       // contextBridge：确保仅暴露必要的 API，避免直接暴露可能带来安全风险的功能。例如，不要暴露对文件系统、网络请求等敏感操作的直接访问。
-      nodeIntegration: false // 禁用 Node.js 集成
+      nodeIntegration: false, // 禁用 Node.js 集成
+       contextIsolation: true,
     }
   })
 
